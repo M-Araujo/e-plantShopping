c6ad7fe92ae79580921af2c0b14a739bf2ba1905
M-Araujo <miriamm_araujo@hotmail.com>
2025-08-18 14:55:43 -0400

    added cart total

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -18,10 +18,12 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
     dispatch(updateQuantity({'item': item, 'action': 'increment'}));
@@ -36,7 +38,6 @@ const CartItem = ({ onContinueShopping }) => {
   } 
 
 
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     return parseFloat(item.cost.substring(1)) * item.quantity;
   };
@@ -66,7 +67,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
