0b07502d8814f05e4ab8326c5d92027865ab4912
M-Araujo <miriamm_araujo@hotmail.com>
2025-08-19 00:31:55 -0400

    added plant category name

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -281,27 +281,30 @@ function ProductList({ onHomeClick }) {
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
-                            <div>
+                            <div className="tag_home_link">
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                             </div>
                         </a>
                     </div>
-
                 </div>
-                <div style={styleObjUl}>
+                <div  style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span className="cart_quantity_count">{totalCartItems()}</span></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
-                <div className="product-grid">
+                <div className="">
                     {plantsArray.map((item) => (
-                        <div key={item.id} className="product-list">
+                        <div key={item.id} className="product-grid">
+                            <div className="plant_heading">
+                                <h1 className="plantname_heading">{item.category}</h1>
+                            </div>
+                            <div className="product-list">
                             {item.plants.map((plant, index) => (
                                 <div key={`item-${index}`} className="product-card">
-                                    <div className="product-name"> {plant.name}</div>
-                                    <div className="product-image"><img src={plant.image} alt="{plant.name}" /> </div>
+                                    <div className="product-title"> {plant.name}</div>
+                                    <img className="product-image" src={plant.image} alt="{plant.name}" /> 
                                     <div className="product-description"> {plant.description}</div>
                                     <div className="product-price"> {plant.cost}</div>
 
@@ -318,7 +321,9 @@ function ProductList({ onHomeClick }) {
                                     }
 
                                 </div>
+                               
                             ))}
+                                </div>
                         </div>
                     ))}
 
